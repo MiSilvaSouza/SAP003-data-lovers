3339300130ed8e3b68b061e29965a14ca93d6dc4
JessikaFujimura <jessikafujimura@gmail.com>
2019-09-13 13:47:38 -0300

    Adicionado a lista no HTML, eventos de DOM no main JS e função AllChampions no data js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-document.getElementById("button").addEventListener("click", Change);
-
-function Change() {
-    event.preventDefault();
-    document.getElementById("Champions").innerHTML = ChampionsAssassin();
+window.onload = function () {
+    allChampions();
+    for (i in nameChampions) {
+        let element = document.createElement("LI")
+        let contElement = document.createTextNode(nameChampions[i])
+        element.appendChild(contElement);
+        document.getElementById("name").appendChild(element)
+    }
+    for (j in imgChampions) {
+        let image = document.createElement("LI")
+        image.innerHTML = `<img src="${imgChampions[j]}"/>`
+        document.getElementById("imagem").appendChild(image);
+    }
 }
+
+
