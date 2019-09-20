df4d355f892efb87962fa154355ead5be5217838
JessikaFujimura <jessikafujimura@gmail.com>
2019-09-20 11:27:46 -0300

    Filtrando, Ordenando e Calculando por level, css responsivo

--- src/main.js
+++ src/main.js
@@ -1,113 +1,45 @@
 
-window.onload = function () {
-    allChampions();
-    for (let i = 0; i<=name.length && i<img.length; i++) {
-    let element = document.createElement("P")
-    element.innerHTML = `${name[i]}` +`<img class="Avatar" src="${img[i]}"/>`
-    document.getElementById("Champions").appendChild(element)
-    }
-}
-document.getElementById("btn").addEventListener("click", Filter);
-function Filter(option) {
-    document.getElementById("Champions").innerHTML="";
-    filterFunction();
-    option = document.getElementById("option").selectedIndex;
-    if (option == 0) {
-        for (let i = 0; i<=AssassinChampions.length; i++) {
-            let element = document.createElement("P")
-            let info = Object.values(AssassinChampions[i]);
-            console.log(info)
-            element.innerHTML = `${info[0]}` +`<img class="Avatar" src="${info[1]}"/>`+`vida = ${info[2]} <br>`+`dano = ${info[3]}<br>`+`vel.Ataque = ${info[4]}<br>`+`vel.Mov. = ${info[5]}<br>`+ `reg.Vida = ${info[6]}<br>` +`armadura = ${info[7]}`
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
-    if (option == 1) {
-        console.log(TankChampions)
-        for (let i = 0; i<=TankChampions.length; i++) {
-            let element = document.createElement("P")
-            let info = Object.values(TankChampions[i]);
-            console.log(info)
-            element.innerHTML = `${info[0]}` +`<img class="Avatar" src="${info[1]}"/>`+`vida = ${info[2]} <br>`+`dano = ${info[3]}<br>`+`vel.Ataque = ${info[4]}<br>`+`vel.Mov. = ${info[5]}<br>`+ `reg.Vida = ${info[6]}<br>` +`armadura = ${info[7]}` 
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
-    if (option == 2) {
-        console.log(FighterChampions)
-        for (let i = 0; i<=infoChampions.length; i++) {
-            let element = document.createElement("P")
-            element.innerHTML = `${Object.entries(FighterChampions[i])}` 
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
-    if (option == 3) {
-        console.log(MageChampions)
-        for (let i = 0; i<=infoChampions.length; i++) {
-            let element = document.createElement("P")
-            element.innerHTML = `${Object.entries(MageChampions[i])}` 
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
-    if (option == 4) {
-        console.log(MarksmanChampions)
-        for (let i = 0; i<=infoChampions.length; i++) {
-            let element = document.createElement("P")
-            element.innerHTML = `${Object.entries(MarksmanChampions[i])}` 
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
-    if (option == 5) {
-        console.log(SuportChampions)
-        for (let i = 0; i<=infoChampions.length; i++) {
-            let element = document.createElement("P")
-            element.innerHTML = `${Object.entries(SuportChampions[i])}` 
-            document.getElementById("Champions").appendChild(element)
-        }
-    }
+window.onload = page();
+function page() {
+  takeChampions();
+  infoChampions.map((i) => {
+    document.getElementById("Champions").innerHTML += `<div class = "cards"> ${i.item}` + `<img class="Avatar" src="${i.img}"</div>`;
+  })
 }
 
+document.getElementById("home").addEventListener("click", () => {
+  document.getElementById("Champions").innerHTML = "";
+  infoChampions.map((i) => {
+    document.getElementById("Champions").innerHTML += `<div class = "cards"> ${i.item}` + `<img class="Avatar" src="${i.img}"</div>`;
+  })
+});
 
+document.getElementById("option").addEventListener("change", filter);
+function filter(option) {
+  filterChampions=[];
+  document.getElementById("Champions").innerHTML = "";
+  option = document.getElementById("option").value;
+  takeChampions(option);
+  filterChampions.map((i) => {
+    document.getElementById("Champions").innerHTML += `<div class = "infoChampions"> ${i.item} <img class="Avatar" src="${i.img}"/> vida = ${i.life} <br>  AD = ${i.damage}<br>  AS = ${i.velAttack}<br>  vel.Mov. = ${i.velMove} <br>  reg.Vida = ${i.regenLife}<br> armadura = ${i.armor} </div>`;
+  });
+}
 
+document.getElementById("higher").addEventListener("change", Ordered);
+function Ordered () {
+  document.getElementById("Champions").innerHTML = "";
+  let selection = document.getElementById("higher").value;
+  let order = filterChampions.sort((a, b) => a[selection] > b[selection] ? -1 : a[selection] < b[selection] ? 1 :0 );
+  order.map((i) => {
+    document.getElementById("Champions").innerHTML += `<div class = "infoChampions"> ${i.item} <img class="Avatar" src="${i.img}"/> vida = ${i.life} <br>  AD = ${i.damage}<br>  AS = ${i.velAttack}<br>  vel.Mov. = ${i.velMove} <br>  reg.Vida = ${i.regenLife}<br> armadura = ${i.armor} </div>`;
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (i in nameChampions) {
-//     let element = document.createElement("LI")
-//     let contElement = document.createTextNode(nameChampions[i])
-//     element.appendChild(contElement);
-//     document.getElementById("Champions").appendChild(element)
-// }
-// for (j in imgChampions) {
-//     let image = document.createElement("LI")
-//     image.innerHTML = `<img src="${imgChampions[j]}"/>`
-//     document.getElementById("Champions").appendChild(image);
-// }
-// name.innerHTML = `${nameChampions}`;
-// name.innerHTML = `<img src= "${imgChampions}"/>`;
-
-
-//     for (j in imgChampions) {
-//     let image = document.createElement("P")
-//     image.innerHTML = `<img src="${imgChampions[j]}"/>`
-//     document.getElementById("Champions").appendChild(image);
-// }
-
-//let contElement = document.createTextNode(nameChampions[i])
-    //element.appendChild(contElement);
+document.getElementById("level").addEventListener("change", level);
+function level () {
+  document.getElementById("Champions").innerHTML = "";
+  let selection = Number(document.getElementById("level").value);
+  filterChampions.map((i) => {
+    document.getElementById("Champions").innerHTML += `<div class = "infoChampions"> ${i.item} <img class="Avatar" src="${i.img}"/> vida = ${((i.life)+(selection*(i.lifeLevel))).toFixed(2)} <br> AD = ${((i.damage)+(selection*(i.damageLevel))).toFixed(2)}<br>  AS = ${((i.velAttack)*(selection*(i.velAttackLevel/100))).toFixed(2)}<br>  vel.Mov. = ${i.velMove} <br>  reg.Vida = ${((i.regenLife)+(selection*(i.regenLifeLevel))).toFixed(2)}<br> armadura = ${((i.armor)+(selection*(i.armorLevel))).toFixed(2)}</div>`;
+  });
+}
