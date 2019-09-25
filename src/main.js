a49f8de46fdd2493be6a5942662de32e997b41cc
MiSilvaSouza <mirian.s2@hotmail.com>
2019-09-25 09:46:13 -0300

    versão com história do usuário 3 e testes realizados

--- src/main.js
+++ src/main.js
@@ -1,33 +1,87 @@
 const dataLOL = LOL.data;
-const lolNames = Object.keys(dataLOL);
-const resultado = document.getElementById("resultado"); 
+const allNames = Object.keys(dataLOL);
+const resultado = document.getElementById("resultado");
+const legenda = document.getElementById("legenda");
 
+//Template que mostra todos os personagens quando abre a página ou clica no botão home
 function template(data) {
+  legenda.innerHTML = "";
   resultado.innerHTML = "";
-  const champions = data.map(function(chave) {          
-    return resultado.innerHTML += `<span><img src="${dataLOL[chave].img}"/><br>`+`${chave}</span>`; 
-  });
-  return champions;
+  return champions = data.map(function(name) {          
+    return resultado.innerHTML += `<span><img src="${dataLOL[name].img}"/><br>`+`${name}</span>`; 
+  });  
 }
-window.onload = template(lolNames);
 
+window.onload = template(allNames);
+document.getElementById("home").onclick = function clicar() {
+  template(allNames);  
+}; 
+
+//Template que mostra personagens filtrados por função escolhida
 function templatePro(data) {
   resultado.innerHTML = "";
-  const champions = data.map(function(chave) {          
-    return resultado.innerHTML += `<span><img src="${dataLOL[chave].img}"/><br>`+`${chave}<br>`+
-    `<br>HP: ${dataLOL[chave].stats.hp}<br>`+`AD: ${dataLOL[chave].stats.attackdamage}<br>`+
-    `AS: ${parseFloat(0.625/(1+(+(dataLOL[chave].stats.attackspeedoffset)))).toFixed(3)}<br>`+
-    `VM: ${dataLOL[chave].stats.movespeed}<br>`+`RV: ${dataLOL[chave].stats.hpregen}<br>`+
-    `AR: ${dataLOL[chave].stats.armor}</br></span>`; 
-  });
-  return champions;
+  legenda.innerHTML = "HP - Vida / AD - Dano de Ataque / AS - Velocidade de Ataque / MS - Velocidade de Movimento / HR- Regeneração de Vida / AR - Armadura";  
+  return champions = data.map(function(name) {          
+    return resultado.innerHTML +=`<span><img src="${dataLOL[name].img}"/><br>`+`${name}<br>`+
+    `<br>HP: ${dataLOL[name].stats.hp.toFixed(1)}<br>`+`AD: ${dataLOL[name].stats.attackdamage.toFixed(2)}<br>`+
+    `AS: ${parseFloat(0.625/(1+(+(dataLOL[name].stats.attackspeedoffset)))).toFixed(3)}<br>`+
+    `MS: ${dataLOL[name].stats.movespeed}<br>`+`HR: ${dataLOL[name].stats.hpregen.toFixed(2)}<br>`+
+    `AR: ${dataLOL[name].stats.armor.toFixed(2)}</br></span>`; 
+  }); 
 }
+//Template que mostra personagens filtrados por função escolhida e mostra características por level
+document.getElementById("seletor").onchange = function selecionado(e) {
+  document.getElementById("ordenar").disabled = false;
+  document.getElementById("level").disabled = false;    
+  let seletor1 = e.target.value;
+  const filtrados = filtro(allNames, dataLOL, seletor1);  
+  
+  document.getElementById("level").onchange = function selecionado (e) {
+    let seletor3 = e.target.value;
 
-document.getElementById("seletor").onchange = function selecionado(e) {    
-  let seletor = e.target.value;
-  const filtrados = selecionados.filtro(lolNames, seletor);  
+    if (seletor3 === "1") {
+      templatePro(filtrados);
+    } else {
+      function templateLvl(data) {      
+        resultado.innerHTML = "";
+        legenda.innerHTML = "HP - Vida / AD - Dano de Ataque / AS - Velocidade de Ataque / MS - Velocidade de Movimento / HR- Regeneração de Vida / AR - Armadura";  
+        return champions = data.map(function(name) {             
+          return resultado.innerHTML +=`<span><img src="${dataLOL[name].img}"/><br>`+`${name}<br>`+
+          `<br>HP: ${calcular(dataLOL[name].stats.hp, dataLOL[name].stats.hpperlevel, seletor3).toFixed(1)}<br>`+
+          `AD: ${calcular(dataLOL[name].stats.attackdamage, dataLOL[name].stats.attackdamageperlevel, seletor3).toFixed(2)}<br>`+
+          `AS: ${calcular(parseFloat(0.625/(1+(dataLOL[name].stats.attackspeedoffset))), dataLOL[name].stats.attackspeedperlevel, seletor3).toFixed(3)}<br>`+
+          `MS: ${dataLOL[name].stats.movespeed}<br>`+
+          `HR: ${calcular(dataLOL[name].stats.hpregen, dataLOL[name].stats.hpregenperlevel, seletor3).toFixed(2)}<br>`+
+          `AR: ${calcular(dataLOL[name].stats.armor, dataLOL[name].stats.armorperlevel, seletor3).toFixed(2)}</br></span>`; 
+        });
 
-  templatePro(filtrados);
-    
+      }      
+      templateLvl(filtrados);
+    }
+   
+  };
+  //Template que mostra personagens filtrados e ordenados por característica escolhida
+  document.getElementById("ordenar").onchange = function selecionado (e) {    
+    let seletor2 = e.target.value;
+
+    if (seletor2 === "attackspeedoffset") {
+      const ordenados = filtrados.map(function(elem) {
+        return (([elem, (parseFloat(0.625/(1+(+(mapInfo(filtrados, dataLOL, seletor2)[filtrados.indexOf(elem)]))))).toFixed(3)])); 
+      });                   
+      const ordenaInfo = ordenar(ordenados);
+      const ordenaNames = ordenaInfo.map(elem => elem[0]);
+      templatePro(ordenaNames);
+
+    } else {
+      const ordenados = filtrados.map(function(elem) {
+        return (([elem, mapInfo(filtrados, dataLOL, seletor2)[filtrados.indexOf(elem)]])); 
+      });                      
+      const ordenaInfo = ordenar(ordenados);      
+      const ordenaNames = ordenaInfo.map(elem => elem[0]);
+      templatePro(ordenaNames);
+    };      
+                
+  };
+  return templatePro(filtrados);
 };
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,11 +1,41 @@
 require("../src/data.js");
 
-describe("example", () => {
-  it("is a function", () => {
-    expect(typeof example).toBe("function");
+describe("filtro", () => {
+  it("é uma função", () => {
+    expect(typeof data.filtro).toBe("function");
   });
 
-  it("returns `example`", () => {
-    expect(example()).toBe("example");
+  it("retorna `nome do campeão`", () => {
+    expect(data.filtro(["Aatrox", "Ahri"], {"Aatrox": {"tags": ["Fighter", "Tank"]}, "Ahri": {"tags": ["Mage", "Assassin"]}}, "Mage")).toEqual(["Ahri"]);
+  });
+});
+
+describe("mapInfo", () => {
+  it("é uma função", () => {
+    expect(typeof data.mapInfo).toBe("function");
+  });
+
+  it("retorna `valor do item selecionado`", () => {
+    expect(data.mapInfo(["Aatrox", "Ahri"], {"Aatrox": {"stats": {"hp": 537.8, "hpperlevel": 85, "mp": 105.6, }}, "Ahri": {"stats": {"hp": 514.4, "hpperlevel": 80, "mp": 334, }}}, "hp")).toEqual([537.8, 514.4]);
+  });
+});
+
+describe("ordenar", () => {
+  it("é uma função", () => {
+    expect(typeof data.ordenar).toBe("function");
+  });
+
+  it("retorna `valores da array na posição 1 em ordem decrescente`", () => {
+    expect(data.ordenar([["Vayne", 498.44], ["Urgot", 586.52], ["Jayce", 571.2], ["Azir", 524.4]])).toEqual([["Urgot", 586.52], ["Jayce", 571.2], ["Azir", 524.4], ["Vayne", 498.44]]);
+  });
+});
+
+describe("calcular", () => {
+  it("é uma função", () => {
+    expect(typeof data.calcular).toBe("function");
+  });
+
+  it("retorna `valor calculado por level`", () => {
+    expect(data.calcular(537.8, 85, 2)).toEqual(707.8);
   });
 });
