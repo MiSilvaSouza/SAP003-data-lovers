a6b15d7c35f210be87962bb679e12a6e8e371782
MiSilvaSouza <mirian.s2@hotmail.com>
2019-10-03 13:39:15 -0300

    alterações após feedback

--- src/main.js
+++ src/main.js
@@ -1,61 +1,76 @@
 const resultado = document.getElementById("resultado");
 const legenda = document.getElementById("legenda");
-let infoChampions = [];
 
-Object.keys(LOL.data).map(function(name) {  
-  let nome = name;
-  let img = (LOL.data[name].img);
-  let tag = (LOL.data[name].tags);
-  let hp = LOL.data[name].stats.hp.toFixed(1);
-  let hplvl = LOL.data[name].stats.hpperlevel;
-  let ad = LOL.data[name].stats.attackdamage.toFixed(2);
-  let adlvl = LOL.data[name].stats.attackdamageperlevel;
-  let as = parseFloat(0.625/(1+(+(LOL.data[name].stats.attackspeedoffset)))).toFixed(3);
-  let aslvl = LOL.data[name].stats.attackspeedperlevel;
-  let ms = LOL.data[name].stats.movespeed; 
-  let hr = LOL.data[name].stats.hpregen.toFixed(2);
-  let hrlvl = LOL.data[name].stats.hpregenperlevel;
-  let ar = LOL.data[name].stats.armor.toFixed(2);
-  let arlvl = LOL.data[name].stats.armorperlevel;
-
-  infoChampions.push({img, nome, tag, hp, hplvl, ad, adlvl, as, aslvl, ms, hr, hrlvl, ar, arlvl});
+const infoChampions = Object.keys(LOL.data).map(function(name) { 
+  return {
+    nome: name,
+    img: (LOL.data[name].img),
+    tag: (LOL.data[name].tags),
+    hp: LOL.data[name].stats.hp.toFixed(1),
+    hplvl: LOL.data[name].stats.hpperlevel,
+    ad: LOL.data[name].stats.attackdamage.toFixed(2),
+    adlvl: LOL.data[name].stats.attackdamageperlevel,
+    as: parseFloat(0.625/(1+(+(LOL.data[name].stats.attackspeedoffset)))).toFixed(3),
+    aslvl: LOL.data[name].stats.attackspeedperlevel,
+    ms: LOL.data[name].stats.movespeed, 
+    hr: LOL.data[name].stats.hpregen.toFixed(2),
+    hrlvl: LOL.data[name].stats.hpregenperlevel,
+    ar: LOL.data[name].stats.armor.toFixed(2),
+    arlvl: LOL.data[name].stats.armorperlevel,
+  };
 });
 
-//Template que mostra todos os personagens quando abre a página ou clica no botão home
 function template(data) {  
   legenda.innerHTML = "";
-  resultado.innerHTML = "";
-  data.map(function(item) {          
-    return resultado.innerHTML += `<span><img src="${item.img}"/><br>`+`${item.nome}</span>`; 
-  });  
-}
+  resultado.innerHTML = data.map(function(item) {          
+    return `
+    <span>
+    <img src="${item.img}"/><br>`+`${item.nome}
+    </span>
+    `; 
+  }).join("");    
+};
 
 window.onload = template(infoChampions);
 document.getElementById("home").onclick = function clicar () {
   template(infoChampions);
 };
 
-//Template que mostra personagens filtrados por função escolhida
-function templatePro(data) {
-  resultado.innerHTML = "";
-  legenda.innerHTML = "HP - Vida / AD - Dano de Ataque / AS - Velocidade de Ataque / MS - Velocidade de Movimento / HR- Regeneração de Vida / AR - Armadura";  
-  data.map(function(item) {          
-    return resultado.innerHTML +=`<span><img src="${item.img}"/><br>`+`${item.nome}<br>`+`<br>HP: ${item.hp}<br>`+`AD: ${item.ad}<br>`+`AS: ${item.as}<br>`+`MS: ${item.ms}<br>`+`HR: ${item.hr}<br>`+`AR: ${item.ar}</br></span>`; 
-  }); 
-}
-//Template que mostra personagens filtrados e ordenados por característica escolhida
+function templatePro(data) {  
+  legenda.innerHTML = "HP - Vida "+
+  "/ AD - Dano de Ataque "+
+  "/ AS - Velocidade de Ataque "+
+  "/ MS - Velocidade de Movimento "+
+  "/ HR- Regeneração de Vida "+
+  "/ AR - Armadura ";
+
+  resultado.innerHTML = data.map(function(item) {          
+    return `
+    <span>
+    <img src="${item.img}"/><br>`+`${item.nome}<br><br>`+
+    `HP: ${item.hp}<br>`+
+    `AD: ${item.ad}<br>`+
+    `AS: ${item.as}<br>`+
+    `MS: ${item.ms}<br>`+
+    `HR: ${item.hr}<br>`+
+    `AR: ${item.ar}</br>
+    </span>
+    `; 
+  }).join(""); 
+};
+
 document.getElementById("seletor").onchange = function selecionado(e) {
   document.getElementById("ordenar").disabled = false;
   document.getElementById("escolher").disabled = false;
   document.getElementById("level").disabled = false;    
-  let seletor1 = e.target.value;
+  const seletor1 = e.target.value;
   const filtrados = filtro(infoChampions, seletor1);    
   templatePro(filtrados);
   
   document.getElementById("ordenar").onchange = function selecionado(e) {
-    let seletor2 = e.target.value;    
+    const seletor2 = e.target.value;    
     document.getElementById("escolher").onchange = function selecionado(e) {
-      let seletor3 = e.target.value;
+      const seletor3 = e.target.value;
       
       if (seletor3 === "decrescente") {        
         templatePro(ordenamaior(filtrados, seletor2));
@@ -64,15 +79,30 @@ document.getElementById("seletor").onchange = function selecionado(e) {
       };
     };
   };
-  //Template que mostra personagens filtrados por função escolhida e mostra características por level
+
   document.getElementById("level").onchange = function selecionado (e) {
     let seletor4 = e.target.value;
-    function templateLvl (data) {
-      resultado.innerHTML = "";
-      legenda.innerHTML = "HP - Vida / AD - Dano de Ataque / AS - Velocidade de Ataque / MS - Velocidade de Movimento / HR- Regeneração de Vida / AR - Armadura";  
-      data.map(function(item) {
-        return resultado.innerHTML +=`<span><img src="${item.img}"/><br>`+`${item.nome}<br>`+`<br>HP: ${calcular(+item.hp, +item.hplvl, +seletor4).toFixed(1)}<br>`+`AD: ${calcular(+item.ad, +item.adlvl, +seletor4).toFixed(2)}<br>`+`AS: ${calcularAS(+item.as, +item.aslvl, +seletor4).toFixed(3)}<br>`+`MS: ${item.ms}<br>`+`HR: ${calcular(+item.hr, +item.hrlvl, +seletor4).toFixed(2)}<br>`+`AR: ${calcular(+item.ar, +item.arlvl, +seletor4).toFixed(2)}</br></span>`; 
-      });
+    function templateLvl (data) {      
+      legenda.innerHTML = "HP - Vida "+
+      "/ AD - Dano de Ataque "+
+      "/ AS - Velocidade de Ataque "+
+      "/ MS - Velocidade de Movimento "+
+      "/ HR- Regeneração de Vida "+
+      "/ AR - Armadura ";
+        
+      resultado.innerHTML = data.map(function(item) {
+        return `
+        <span>
+        <img src="${item.img}"/><br>`+`${item.nome}<br><br>`+
+        `HP: ${calcular(+item.hp, +item.hplvl, +seletor4).toFixed(1)}<br>`+
+        `AD: ${calcular(+item.ad, +item.adlvl, +seletor4).toFixed(2)}<br>`+
+        `AS: ${calcularAS(+item.as, +item.aslvl, +seletor4).toFixed(3)}<br>`+
+        `MS: ${item.ms}<br>`+
+        `HR: ${calcular(+item.hr, +item.hrlvl, +seletor4).toFixed(2)}<br>`+
+        `AR: ${calcular(+item.ar, +item.arlvl, +seletor4).toFixed(2)}</br>
+        </span>
+        `; 
+      }).join("");
     };
     templateLvl(filtrados);  
   };
